bcb19c0d39b956d1bec008241b3662131eff8c35
laamaai <maiderp1a@gmail.com>
2017-10-04 18:30:22 +0200

    Primera y segunda pantalla

--- app/src/main/java/com/example/maider/pamplona_negra/MainActivity.java
+++ app/src/main/java/com/example/maider/pamplona_negra/MainActivity.java
@@ -2,12 +2,24 @@ package com.example.maider.pamplona_negra;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.widget.Button;
+import android.view.View;
+import android.content.Intent;
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn = (Button) findViewById(R.id.btnJugar);
+        btn.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Intent intent = new Intent (v.getContext(), PresentacionJuego.class);
+                startActivityForResult(intent,0);
+            }
+        });
+
+
     }
 }
---
+++ app/src/main/java/com/example/maider/pamplona_negra/PresentacionJuego.java
@@ -0,0 +1,13 @@
+package com.example.maider.pamplona_negra;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PresentacionJuego extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_presentacion_juego);
+    }
+}
