c908d5bd88cb0212e6f22458844098478be2f076
laamaai <maiderp1a@gmail.com>
2017-10-08 12:55:19 +0200

    Splash hecho y funcionando e importar tipo de letra de google fonts

--- app/src/main/java/com/example/maider/pamplona_negra/MainActivity.java
+++ app/src/main/java/com/example/maider/pamplona_negra/MainActivity.java
@@ -1,16 +1,26 @@
 package com.example.maider.pamplona_negra;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.view.View;
 import android.content.Intent;
+import android.widget.TextView;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+       TextView nuevaFuente = (TextView)findViewById(R.id.txtV);
+
+        Typeface face = Typeface.createFromAsset(getAssets(),"Nosifer.ttf");
+       ((TextView) findViewById(R.id.txtV)).setTypeface(face);
+
 
         Button btn = (Button) findViewById(R.id.btnJugar);
         btn.setOnClickListener(new View.OnClickListener(){
