c4e7ec66c036c1174eca50251ffc2a9f404816df
laamaai <maiderp1a@gmail.com>
2017-10-09 17:03:57 +0200

    No se que cambios he hecho

--- app/src/main/java/com/example/maider/pamplona_negra/Splash.java
+++ app/src/main/java/com/example/maider/pamplona_negra/Splash.java
@@ -7,7 +7,7 @@ import android.os.Bundle;
 
 public class Splash extends AppCompatActivity {
 
-    private static int SPLASH_TIME_OUT = 4000;
+    private static int SPLASH_TIME_OUT = 5040;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
